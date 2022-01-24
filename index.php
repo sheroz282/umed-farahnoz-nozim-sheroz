b75b537f436e068e743837d0ea7eae9f2d839d5a
academy-php <zoidovsheroz46@gamil.com>
2022-01-24 20:54:27 +0500

    Add index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+/*
+	Recipe for sushi
+	
+	1) 2 cups Japanese rice
+	2) trout,
+	3) cucumber,
+	4) 2 tbsp japanese vinegar,
+	5) 2.5 tbsp sugar,
+	6) 2 tsp salt,
+	7) wasabi,
+	8) nori,
+	9) gary,
+	10) soy sauce.
+
+*/
+
+	function sushi(
+		int $japanese_rice, 
+		int $trout, 
+		int $cucumber, 
+		int $japanese_vinegar, 
+		float $sugar, 
+		int $salt, 
+		int $wasabi, 
+		int $nori,
+		int $gary,
+		int $soy_sauce)
+	{
+		echo "Приготовьте ингредиенты <br/> 
+				$japanese_rice стакана рис <br/> 
+				Добавляем 2 стакан воду <br/> 
+				Варить рис 15-20 <br> 
+				Снять с огня и постоять 10 минут <br/>
+				Добавляем 2 ст.л уксуса, добавляем 2,5 ст.л сахара и 2 ч.л соли <br/>
+				Начинаем скручивать <br/>
+				будеть готова!
+				";
+		/*
+			Rinse rice thoroughly under cold water until clear water begins to drain from it - rice powder should be washed
+			Then pour rice with cold water in a ratio of 1: 1. Those. if we have taken 2 cups of rice, then it must be poured with 2 cups of water.
+			Bring the rice to a boil, close the lid and cook over high heat for 1 minute. Then reduce the heat and cook the rice for 15-20 minutes until the water has completely evaporated.
+			Remove from heat and, without opening the lid, let stand for 10 minutes.
+			Meanwhile, while the rice is standing, prepare the rice dressing mixture. To do this, in 2 tbsp. stir Japanese vinegar until almost completely dissolved 2.5 tbsp. sugar and 2 tsp. salt.
+			Remove from heat and, without opening the lid, let stand for 10 minutes.
+			Meanwhile, while the rice is standing, prepare the rice dressing mixture. To do this, in 2 tbsp. stir Japanese vinegar until almost completely dissolved 2.5 tbsp. sugar and 2 tsp. salt.
+			Put the rice in a large wide dish and - attention! – Without stirring, add the prepared mixture to the rice. Spread it evenly by gently turning the rice in quick motions with a wooden spatula. Otherwise, the rice may turn into porridge.
+			Allow the rice to cool slightly so that you can continue to take it with your hands.
+			Now we will twist the rolls. To do this, put a sheet of nori on the makushi with the pimply side up, and horizontal lines on the nori parallel to the bamboo stalks. Do not be afraid that nori is so dry and brittle. When rice gets on them, they will become very elastic and will curl up perfectly.
+			Spread with hands dipped in cold water, rice. This is done so that the rice does not stick to the hands. You need to wet your hands every time before taking a new portion of rice.
+			We evenly distribute the rice over the nori, leaving about 0.5-1 cm on one edge so that the rice does not interfere with fastening the nori after twisting.
+			Apply a thin layer of wasabi in the middle. Attention!!!! Wasabi is very spicy, you need to apply it very thinly, carefully smearing it.
+			Trout and cucumbers cut into thin rectangles.
+			Lay out in Fig.
+			We start twisting with a bamboo rug.
+			This is roughly how it works. You need to twist it very tightly so that there are no voids with air.
+			After twisting, take the next sheet of nori and the next portion of rice. We repeat everything until the rice is finished.
+			It turns out such long twisted rolls. They should feel firm to the touch, not soft.
+			Next, take the sharpest knife and cut into 6-7 pieces. The knife must be washed under cold water each time, otherwise the rice will stick and interfere with cutting.
+		*/
+	}
+	sushi(2, 1, 1, 2, 2.5, 2, 1, 1, 1, 1);
