98219558504afcb370e93ac2bbf9d7b5deb3c71e
Nozim <nozim.safarzoda@gmail.com>
2022-01-24 20:53:50 +0500

    index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+qandchoy Recipe
+1 100 ml hot water
+2 30 ml sugar
+*/
+function qandchoy(int $water, int $sugar){
+    echo
+    "
+    1. $water ml obro dar tefal garm mekunem <br>
+    2. $sugar gr shakarro dar daruni ob andokhta mekobem<br>
+    ";
+}
+qandchoy(100, 30);
